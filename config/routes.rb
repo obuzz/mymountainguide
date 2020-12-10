3080f8f3f2a3f55ef039f37c59b006c93e49434e
Owen Prew <odprew@gmail.com>
2020-12-10 15:51:46 +0000

    added bootstrap and some le wagon components to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/home', to: "pages#home"
+  
+  root to: 'pages#home'
+  get '/home', to: "pages#guides"
   get '/about', to: "pages#about"
   get '/contact', to: "pages#contact"
 end
